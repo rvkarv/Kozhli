90acba0e2a3483b1b889815afb44f6afc99bbe96
rvkarv <165048267+rvkarv@users.noreply.github.com>
2026-08-16 10:10:21 +0530

    Align final Moon regression with latest Master Workbook timestamps

--- panchapakshi_app/test/master_workbook_alignment_final_regression_test.dart
+++ panchapakshi_app/test/master_workbook_alignment_final_regression_test.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:panchapakshi_app/core/moon_nakshatra_window.dart';
 
-// Final authoritative Master Workbook boundary regression.
+// Authoritative Master Workbook regression for Rajahmundry, 16-Aug-2026.
+// Values are transcribed from the latest refreshed Workbook, in UTC.
 void main() {
-  test('Master Workbook: Rajahmundry 15-Aug-2026 four Moon boundaries', () {
+  test('Master Workbook: Rajahmundry Hasta/Pada 2 boundaries', () {
+    // 10:00 IST = 04:30 UTC, safely inside Hasta Pada 2.
     final window = MoonNakshatraWindow.forUtc(
-      DateTime.utc(2026, 8, 15, 6, 46),
+      DateTime.utc(2026, 8, 16, 4, 30),
     );
 
-    _near(window.startUtc, DateTime.utc(2026, 8, 14, 22, 13, 18, 742000));
-    _near(window.endUtc, DateTime.utc(2026, 8, 15, 21, 56, 3, 156000));
-    _near(window.padaStartUtc, DateTime.utc(2026, 8, 15, 4, 5, 12, 797000));
-    _near(window.padaEndUtc, DateTime.utc(2026, 8, 15, 9, 59, 37, 906000));
+    _near(window.startUtc, DateTime.utc(2026, 8, 15, 21, 56, 12, 884000));
+    _near(window.endUtc, DateTime.utc(2026, 8, 16, 22, 21, 4, 425000));
+    _near(window.padaStartUtc, DateTime.utc(2026, 8, 16, 3, 58, 26, 726000));
+    _near(window.padaEndUtc, DateTime.utc(2026, 8, 16, 10, 3, 21, 647000));
   });
 }
 
